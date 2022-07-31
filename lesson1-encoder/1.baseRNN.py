26ea8b020fee5d3ae3ec955327a88e65721eb1a9
slatter666 <1162247688@qq.com>
2022-07-31 18:31:52 +0800

    Encoder 第一个实例完成

--- lesson1-encoder/1.baseRNN.py
+++ lesson1-encoder/1.baseRNN.py
@@ -14,7 +14,7 @@ import torch
 from torch import nn
 import unicodedata
 
-print("---------------------Set hyperpameters---------------------")
+print("---------------------Set hyperparameters---------------------")   # 更改：parameters拼写错误已更改
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 hidden_size = 64
 learning_rate = 0.5
